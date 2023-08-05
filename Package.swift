eda5d48b3de370f2059984d4e2a5df081983768f
William Thomas <williamt@ifit.com>
2023-08-05 00:09:47 -0700

    Setting iOS 13 support

--- Package.swift
+++ Package.swift
@@ -3,7 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftySensors",
-    platforms: [.macOS(.v10_13), .iOS(.v9), .tvOS(.v9), .watchOS(.v4)],
+    platforms: [
+            .iOS(.v13),
+        ],
     products: [
         .library(name: "SwiftySensors", targets: ["SwiftySensors"]),
     ],
